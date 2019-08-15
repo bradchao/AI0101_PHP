b4f43927129d5aab9e03d8098e187c734779d88d
Brad <brad@brad.tw>
2019-08-15 16:07:04 +0800

    151607

--- brad06.php
+++ brad06.php
@@ -1,3 +1,11 @@
+<?php
+    $result = $x = $y = '';
+    if (isset($_GET['x'])) {
+        $x = $_GET['x']; $y = $_GET['y'];
+        $result = $x + $y;
+        //echo "{$x} + {$y} = {$result}";
+    } 
+?>
 <script>
     function cal(){
         let x = document.getElementById('x').value;
@@ -6,11 +14,11 @@
         document.getElementById('here').innerHTML = result;
     }
 </script>
-<form action="brad07.php">
-    <input id="x" name="x">
+<form action="brad06.php">
+    <input id="x" name="x" value="<?= $x ?>">
     +
-    <input id="y" name="y">
+    <input id="y" name="y" value="<?php echo $y; ?>">
     <input type="submit" value="=">
     <input type="button" onclick="cal()" value="=">
-    <span id='here'></span>
+    <span id='here'><?php echo $result; ?></span>
 </form>
